f0750a203106bb7dc58559d351b933dd1aa4058d
Mikhail Diachenko <xdiach00@vutbr.cz>
2024-07-14 14:17:38 +0200

    Add Detekt with gradle tasks

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,39 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
+    alias(libs.plugins.detekt) apply true
+}
+
+tasks.register("detektAll", io.gitlab.arturbosch.detekt.Detekt::class) {
+    val autoFix = true //project.hasProperty("detektAutoFix")
+
+    description = "Custom DETEKT build for all modules from MeguMethod"
+    parallel = true
+    ignoreFailures = false
+    autoCorrect = autoFix
+    buildUponDefaultConfig = true
+    setSource(file(projectDir))
+    config.setFrom(files("$rootDir/config/detekt/detekt.yml"))
+    baseline.set(file("$rootDir/config/detekt/baseline.xml"))
+    include("**/*.kt")
+    exclude("**/resources/**", "**/build/**")
+    reports {
+        html.required.set(true)
+        xml.required.set(true)
+        txt.required.set(true)
+    }
+}
+
+tasks.register("detektBaselineAll", io.gitlab.arturbosch.detekt.DetektCreateBaselineTask::class) {
+    description = "Custom DETEKT baseline for all modules"
+    setSource(file(projectDir))
+    config.setFrom(files("$rootDir/config/detekt/detekt.yml"))
+    baseline.set(file("$rootDir/config/detekt/baseline.xml"))
+    include("**/*.kt")
+    exclude("**/resources/**", "**/build/**")
+}
+
+dependencies {
+    detektPlugins(libs.detekt.formatting)
+    detektPlugins(libs.detekt.compose)
 }
